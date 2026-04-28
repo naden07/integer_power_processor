2828cfcd99b839f95f5b5bbba202b4e6e7059f82
jlnshly <jelaineashleyramos@gmail.com>
2026-04-29 01:37:55 +0800

    implement data processor for squares and cubes

--- processor.py
+++ processor.py
@@ -1,2 +1,10 @@
 class DataProcessor:
     """Handles the logic of filtering and transforming integers."""
+
+    def process_evens(self, numbers):
+        #even integers: n^2
+        return [n ** 2 for n in numbers if n % 2 == 0]
+
+    def process_odds(self, numbers):
+        #odd integers: n^3
+        return [n ** 3 for n in numbers if n % 2 != 0]
